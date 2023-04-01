7756937e8bab5ba7bfca63ad01bed49d66650868
Kit <nvmmonkey@github.com>
2023-04-02 02:54:56 +0800

    add memory array storage view for the funders Array

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,13 +3,17 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
     address[] public funders;
+
     receive() external payable {}
 
     function addFunds() external payable {
         funders.push(msg.sender);
     }
 
-    
+    function getAllFunders() external view returns (address[] memory) {
+        return funders;
+        //use view here because the address array alters the storage state
+    }
 
     //truffle console
     //const instance = await Faucet.deployed()
