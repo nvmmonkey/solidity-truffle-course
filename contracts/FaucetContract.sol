94bd2db8deec13a74cf4c11c86de9a3ea27fff01
Kit <nvmmonkey@github.com>
2023-04-02 03:03:31 +0800

    Add find funders with Indexing

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -10,12 +10,20 @@ contract Faucet {
         funders.push(msg.sender);
     }
 
-    function getAllFunders() external view returns (address[] memory) {
+    function getAllFunders() public view returns (address[] memory) {
         return funders;
         //use view here because the address array alters the storage state
+        //public can be call in other functions
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns (address) {
+        address[] memory _funders = getAllFunders();
+        return _funders[index];
     }
 
     //truffle console
     //const instance = await Faucet.deployed()
     //instance.addFunds({value:"$eth gwei"})
+    
+    //e.g. > instance.addFunds({value:"200000", from: accounts[0})
 }
