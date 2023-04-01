3fdde9d9c8a573a238240c97bf95308b089ce6e9
Kit <nvmmonkey@github.com>
2023-04-02 01:55:56 +0800

    Add external payable function into Faucet.sol

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -7,9 +7,19 @@ contract Faucet {
     //not specify function name to call
 
     //External function are part of the contract
-    //which meas they can be called via contracts and other 
+    //which meas they can be called via contracts and other
     //txs
 
     receive() external payable {}
+
+    function addFunds() external payable {}
+
+    //truffle console
+    //const instance = await Faucet.deployed()
+    //instance.addFunds({value:"$eth gwei"})
 }
 
+//Block info
+//Nouce- a hash that when combined with the minHash proofs that
+//the block has gone tru proof of work (POW)
+//8 bytes =>64bits
