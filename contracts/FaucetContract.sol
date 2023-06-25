1efb0535dda90ce1ad2abb5c712cf02abf193618
Kit <nvmmonkey@github.com>
2023-06-25 16:16:42 -0700

    Add For Loop to get all funders in Array Memory

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -15,14 +15,25 @@ contract Faucet {
         funders[index] = msg.sender;
     }
 
+    function getAllFunders() external view returns (address[] memory) {
+        address[] memory _funders = new address[](numberOfFunders);
+
+        for (uint i = 0; i < numberOfFunders; i++) {
+            _funders[i] = funders[i];
+        }
+
+        return _funders;
+    }
+
     function getFunderAtIndex(uint8 index) external view returns (address) {
         return funders[index];
-    } 
+    }
 
     //truffle console
     //const instance = await Faucet.deployed()
-    //instance.addFunds({from:accounts[0], value:"200000000})
-    //instance.addFunds({from:accounts[1], value:"200000000})
+    //instance.addFunds({from:accounts[0], value:"200000000"})
+    //instance.addFunds({from:accounts[1], value:"200000000"})
+    //instance.getFunderAtIndex(0)
 
     //e.g. > instance.addFunds({value:"200000", from: accounts[0})
 }
