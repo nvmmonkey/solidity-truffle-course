faea7983d2fd6c7605f05641ee459f2a660c5c7e
Kit <nvmmonkey@github.com>
2023-06-25 16:27:02 -0700

    Number of funders by boolean value

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
     uint public numberOfFunders;
-    mapping(uint => address) private funders;
+    mapping(address => bool) private funders;
 
     //private -> can be accesible only within the smart contract
     //internal -> can be accesible within smart contract and also derived from smart contract
@@ -11,22 +11,12 @@ contract Faucet {
     receive() external payable {}
 
     function addFunds() external payable {
-        uint index = numberOfFunders++;
-        funders[index] = msg.sender;
-    }
+        address funder = msg.sender;
 
-    function getAllFunders() external view returns (address[] memory) {
-        address[] memory _funders = new address[](numberOfFunders);
-
-        for (uint i = 0; i < numberOfFunders; i++) {
-            _funders[i] = funders[i];
+        if (!funders[funder]) {
+            numberOfFunders++;
+            funders[funder] = true;
         }
-
-        return _funders;
-    }
-
-    function getFunderAtIndex(uint8 index) external view returns (address) {
-        return funders[index];
     }
 
     //truffle console
@@ -34,6 +24,8 @@ contract Faucet {
     //instance.addFunds({from:accounts[0], value:"200000000"})
     //instance.addFunds({from:accounts[1], value:"200000000"})
     //instance.getFunderAtIndex(0)
+
+    //instance.getAllFunders()
 
     //e.g. > instance.addFunds({value:"200000", from: accounts[0})
 }
