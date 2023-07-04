27eb9e9a46823325c35aa3258fc3eb8b76d7f90a
Kit <nvmmonkey@github.com>
2023-07-04 15:49:55 -0700

    Intergrate Web3 into the metamask provider for getting accounts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import Web3 from "web3";
 
 function App() {
+  const [web3API, setWeb3API] = useState({
+    provider: null,
+    web3: null,
+  });
+
   useEffect(() => {
     const loadProvider = async () => {
       //with metamask we have an access to window.ethereum & window.web3
       //metamask injects a global API into website
       //this API allows websites to request users, accounts, read data on blockchain
       //sign messages and transactions
+
+      let provider = null;
+      if (window.ethereum) {
+        provider = window.ethereum;
+
+        try {
+          await provider.enable();
+        } catch {
+          console.error("User denied accounts access!");
+        }
+      } else if (window.web3) {
+        provider = window.web3.currentProvider;
+      } else if (!process.env.production) {
+        provider = new Web3.providers.HttpProvider("http://localhost:7545");
+      }
+
+      setWeb3API({
+        web3: new Web3(provider),
+        provider,
+      });
 
       console.log(window.web3);
       console.log(window.ethereum);
@@ -16,6 +42,8 @@ function App() {
     loadProvider();
   }, []);
 
+  console.log(web3API.web3);
+
   return (
     <>
       <div className="faucet-wrapper">
@@ -23,17 +51,7 @@ function App() {
           <div className="balance-view is-size-2">
             Current Balance: <strong>10</strong>ETH
           </div>
-          <button
-            className="btn mr-2"
-            onClick={async () => {
-              const accounts = await window.ethereum.request({
-                method: "eth_requestAccounts",
-              });
-              console.log(accounts);
-            }}
-          >
-            Enable Ethereum
-          </button>
+
           <button className="btn mr-2">Donate</button>
           <button className="btn">Withdraw</button>
         </div>
