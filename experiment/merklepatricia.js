3c2f7deae129fdc915a75a769504431e5d2065d1
Kit <nvmmonkey@github.com>
2023-07-19 16:05:56 -0700

    Branch node/Leaf Node in Patricia Tree, key nibbles to hash, value from ASCII to hex

--- experiment/merklepatricia.js
+++ experiment/merklepatricia.js
@@ -22,9 +22,23 @@ const traverseTrie = (node) => {
 
 async function test() {
   await trie.put(
-    Buffer.from("32fa7c"),
-    // ASCII Text to Hex 32 30
+    Buffer.from("32fa7b"), //branch node at different Leaf Node
+    // keccak256(key1:32fa7b) =
+    // nibles (decimals) to (Heximals) = hash
+    // hash 3(branch node) +  nibbles(hex) -> 3865e1f181df18d1fff8847c6298e5b2c621a56f368e030e8ead670c8b01aa1
+    Buffer.from("10")
+    // Nibbles decimals(keccak256(key) -> hash ) | Value (buffer: ASCII text to HEX)
+    // ASCII Text to Hex 31 30
+  );
+
+  await trie.put(
+    Buffer.from("32fa7c"), //branch node at different Leaf Node
+    // keccak256(key2:32fa7c) =
+    // nibles (decimals) to (Heximals) = hash
+    // hash 4(branch node) + nibbles(hex) -> f6c1c50fde5f5d4f20c2979974a8f465b24e65062f02ef80f722200f35105e2
     Buffer.from("20")
+    // Nibbles | Value (buffer: ASCII text to HEX)
+    // ASCII Text to Hex 32 30
   );
 
   traverseTrie(trie.root);
@@ -51,6 +65,6 @@ test();
 //0xC0(hex) = 192 (dec) + 37 bytes (length of the payload 74 bits) = 229 (dec) = e5(hex)
 // e5 a1204f6c1c50fde5f5d4f20c2979974a8f465b24e65062f02ef80f722200f35105e2823230
 
-// keccak(RLP) = 
+// keccak(RLP) =
 // e5a1204f6c1c50fde5f5d4f20c2979974a8f465b24e65062f02ef80f722200f35105e2823230
 // roothash = 17dee68b36b0276d8db503b497c8335d5d4ace0ed3fef5f6fa62644dcd66f170
